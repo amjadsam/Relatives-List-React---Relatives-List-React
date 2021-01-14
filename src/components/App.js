ed6485290bb225d4a188ef4dd70e9e77d9b8f223
amjadsam <68984249+amjadsam@users.noreply.github.com>
2021-01-14 09:30:16 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,18 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
+    
     render() {
-
+       let relatives=["amjad","arshad","aijaz"];  
         return(
             <div id="main">
                {/* Do not remove the main div */}
+            <ol key="relativeList">
+             
+             {relatives.map((el,index)=>
+                <li key={"relativeListItem"+(index+1)}>{el}</li>)}
+             </ol>
+            
             </div>
         )
     }
